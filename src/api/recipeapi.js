300d1d7d316d02cc2e24905ef053b2bcdad2cc8b
Karabet Jazmahji <48636226+KarabetJazmahji@users.noreply.github.com>
2022-06-16 00:38:21 -0400

    Update recipeapi.js

--- src/api/recipeapi.js
+++ src/api/recipeapi.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const KEY = "811884e30cmsh504ac0ac8447419p155cc4jsnbd8398b27283";
  
 export default axios.create({
-  baseURL: "https://cors-anywhere.herokuapp.com/http://www.recipepuppy.com/api",
+  baseURL: "http://www.recipepuppy.com/api",
   params: {
       key: KEY
   }
